6f743dfbfc1ed0b0b99eb2cb61bd81d6d9d522bd
daadhaymour <135389387+daadhaymour@users.noreply.github.com>
2025-07-13 14:25:03 -0600

    Update besttimetobuy_sellstock_121.java
    optimized/full solution

--- besttimetobuy_sellstock_121.java
+++ besttimetobuy_sellstock_121.java
@@ -1,42 +1,20 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150
 
-//only passed 111/212 test cases - will re-try later
-
 class Solution {
     public int maxProfit(int[] prices) {
 
-        // keep track of how much we are buying/selling for 
-        int buy = 0;
-        int sell = 0;
+        int buy = prices[0];
+        int profit = 0;
 
-        // two pointers, left indicates previous day and right is the next day
-        int left = 0;
-        int right = 1;
-        int profit = 0; //leep track of current profit
-        int max_profit = 0; //what we want to return
+        for(int i=0; i<prices.length; i++){
+            if(prices[i] < buy){ //if element is less than previous buy value, then prices[i] is the new day to buy tock
+                buy = prices[i];
+            } else if(prices[i]-buy > profit){ //if new day price - buy is greater than current profit; next best day to sell; profit is updated
+                profit = prices[i]-buy;
 
-        while(left < prices.length && right < prices.length){
-        if (prices[right] < prices[left]){
-            buy = prices[right];
-            right++;
-            left++;
-
-        }
-        else{ // when prices[right] > prices [left] aka probable day to sell
-
-            sell = prices[right];
-            profit = sell-buy;
-
-            if(profit > max_profit){
-                max_profit = profit;
             }
-            right++;
         }
-        }
-
-        return max_profit;
-
-
-
-    }
+        return profit;
+        
+}
 }
